8eabe80497a4ae1bc95331a4ad41596cd531fec5
M S I <M S I@MSI>
2024-05-09 23:08:53 +0700

    menambahkan alamat api

--- lib/halaman_input.dart
+++ lib/halaman_input.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Halaman_Input extends StatefulWidget {
   const Halaman_Input({super.key});
@@ -8,6 +11,30 @@ class Halaman_Input extends StatefulWidget {
 }
 
 class _Halaman_InputState extends State<Halaman_Input> {
+  List _listdata = [];
+  bool _loading = true;
+
+  Future _getdata() async {
+    try {
+      final respon =
+          await http.get(Uri.parse('http://192.168.11.160/api_input/read.php'));
+      if (respon.statusCode == 200) {
+        final data = jsonEncode(respon.body);
+        setState(() {
+          _listdata = data as List;
+          _loading = false;
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void initState() {
+    _getdata();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
