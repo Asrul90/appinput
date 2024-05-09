6f000e182653fcd11c5481df03f9af72d915ce4b
M S I <M S I@MSI>
2024-05-10 00:42:49 +0700

    menambahkan globalkey dan api

--- lib/lihat_cafe.dart
+++ lib/lihat_cafe.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LihatCafe extends StatefulWidget {
   const LihatCafe({super.key});
@@ -8,6 +9,22 @@ class LihatCafe extends StatefulWidget {
 }
 
 class _LihatCafeState extends State<LihatCafe> {
+  final formKey = GlobalKey<FormState>();
+  TextEditingController nama = TextEditingController();
+  TextEditingController alamat = TextEditingController();
+
+  Future _simpan() async {
+    final respon = await http
+        .post(Uri.parse('http://192.168.11.160/api_input/create.php'), body: {
+      'nama': nama.text,
+      'alamat': alamat.text,
+    });
+    if (respon.statusCode == 200) {
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
